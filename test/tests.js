3bf6bf53aa5e05db9e302af1cc190c09c8c2ea9f
Carlos Us Santiago <cdus12@gmail.com>
2019-04-26 14:37:11 -0700

    tdd addDonut function

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,25 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+function addDonut(parentElement, donutFlavor) {
+    const span = document.createElement('span');
+    span.classList.add('part');
+    span.classList.add(donutFlavor);
+
+    parentElement.appendChild(span);
+}
+
+test('append selected donut to dog element', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const donutFlavor = 'purple-donut';
+    const dogParentElement = document.createElement('div');
+    const expected = '<span class="part purple-donut"></span>';
 
     //Act 
     // Call the function you're testing and set the result to a const
+    addDonut(dogParentElement, donutFlavor);
 
     //Assert
-    assert.equal(true, false);
+    const parentInnerHTML = dogParentElement.innerHTML; 
+    assert.equal(parentInnerHTML, expected);
 });
