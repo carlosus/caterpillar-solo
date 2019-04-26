cee3d702a47a8d80d938d44228079de8ea978137
Carlos Us Santiago <cdus12@gmail.com>
2019-04-26 15:39:30 -0700

    fixed syntax

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,18 @@
-import addDonut from '../src/add-donut.js'
+import addDonut from '../src/add-donut.js';
 const test = QUnit.test;
  
 test('append selected donut to dog element', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const donutFlavor = 'purple-donut';
-    const dogParentElement = document.createElement('div');
+    const mockDog = document.createElement('div');
     const expected = '<span class="part purple-donut"></span>';
 
     //Act 
     // Call the function you're testing and set the result to a const
-    addDonut(dogParentElement, donutFlavor);
+    addDonut(mockDog, donutFlavor);
 
     //Assert
-    const parentInnerHTML = dogParentElement.innerHTML; 
+    const parentInnerHTML = mockDog.innerHTML; 
     assert.equal(parentInnerHTML, expected);
-})
+});
